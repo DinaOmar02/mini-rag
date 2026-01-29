537879eb548bd0ba36989b1d080eac1c905c8037
Dina <dinaomar489@gmail.com>
2026-01-29 18:25:06 +0200

    add settigs class to config.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
-load_dotenv(".env")
+
+#from dotenv import load_dotenv  replaced by helpers/config.py
+#load_dotenv(".env")   replaced by helpers/config.py
 
 import os
 from routes import base
