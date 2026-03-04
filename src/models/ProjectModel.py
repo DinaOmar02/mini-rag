41e7c82b9fbb1268e577de95ae9800b5bafdd832
Dina Omar <dinaomar489@gmail.com>
2026-03-04 16:25:52 +0200

    create DataBaseEnum

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -3,6 +3,5 @@ from .db_schemes import Project
 
 class ProjectModel(BaseDataModel):
 
-    def __init__(self):
-        super().__init__()
-        
+    def __init__(self, db_client:object):
+        super().__init__(db_client = db_client)
---
+++ src/models/enums/DataBaseEnum.py
@@ -0,0 +1,8 @@
+from enum import Enum
+from typing import Collection
+
+class DataBaseEnum:
+
+    COLLECTION_PROJECT_NAME = "projects"
+    COLLECTION_CHUNK_NAME = "chunks"
+    
