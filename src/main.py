a667d4eb3a3571f7d6641175ffe681ceb99384d0
Dina Omar <dinaomar489@gmail.com>
2026-05-24 18:25:47 +0300

    create generation and embedding clients

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers import get_settings
+from .stores.llm.LLMProviderFactory import LLMProviderFactory
 
 #from dotenv import load_dotenv  replaced by helpers/config.py
 #load_dotenv(".env")   replaced by helpers/config.py
@@ -10,18 +11,30 @@ from routes import base, data
 
 app = FastAPI()
 
-@app.on_event("startup")
+#@app.on_event("startup")
 async def startup_db_client():
     settings = get_settings()
     app.mongo_connect= AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client= app.mongo_connect[settings.MONGODB_DATABASE]
 
+    llm_provider_factory = LLMProviderFactory(settings)
 
-@app.on_event("shutdown")
+    # generation client
+    app.generation_client = llm_provider_factory.create(provider = settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model_id(settings.GENERATION_MODEL_ID)
+
+    # embedding client
+    app.embedding_client = llm_provider_factory.create(provider = settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model_id(settings.EMBEDDING_MODEL_ID, 
+                                                embedding_size = settings.EMBEDDING_MODEL_SIZE
+    )
+
+#@app.on_event("shutdown")
 async def shutdown_db_client():
     app.mongo_connect.close()
 
-
+app.router.lifespan.on_startup.append(startup_db_client)
+app.router.lifespan.on_shutdown.append(shutdown_db_client)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
