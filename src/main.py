0010ec72a407ed7f635ff2eb59f3f9d092b006ac
Dina Omar <dinaomar489@gmail.com>
2026-09-22 19:16:53 +0300

    migrate application to PostgreSQL

--- src/main.py
+++ src/main.py
@@ -6,20 +6,29 @@ from stores.llm.LLMProviderFactory import LLMProviderFactory
 from stores.vectordb.vectorDBFactory import VectorDBFactory
 from stores.llm.templates.template_parser import TemplateParser
 
-
 #from dotenv import load_dotenv  replaced by helpers/config.py
 #load_dotenv(".env")   replaced by helpers/config.py
 
 import os
 from routes import base, data, nlp
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+from utils.metrics import setup_metrics
 
 app = FastAPI()
 
+# Setup Prometheus metrics
+setup_metrics(app)
 
 async def startup_span():
     settings = get_settings()
-    app.mongo_connect= AsyncIOMotorClient(settings.MONGODB_URL)
-    app.db_client= app.mongo_connect[settings.MONGODB_DATABASE]
+
+    # create async engine for PostgreSQL
+    postgres_conn = f"postgresql+asyncpg://{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_MAIN_DATABASE}"
+    app.db_engine = create_async_engine(postgres_conn)
+
+    app.db_client= sessionmaker(app.db_engine, class_=AsyncSession, expire_on_commit=False)
 
     llm_provider_factory = LLMProviderFactory(settings)
 
@@ -34,18 +43,18 @@ async def startup_span():
     )
 
     # vector db client
-    vector_db_factory = VectorDBFactory(settings)
+    vector_db_factory = VectorDBFactory(settings, db_client=app.db_client)
 
     app.vector_db_client = vector_db_factory.create(
     provider=settings.VECTOR_DB_BACKEND
     )
-    app.vector_db_client.connect()
+    await app.vector_db_client.connect()
 
     app.template_parser = TemplateParser(language = settings.PRIMARY_LANG, default_language = settings.DEFAULT_LANG)
 
 
 async def shutdown_span():
-    app.mongo_connect.close()
+    app.db_engine.dispose()
     app.vector_db_client.disconnect()
 
 
