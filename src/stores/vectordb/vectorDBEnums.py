498223ed1de617df3822c1890a4aa6f88d6eb4b7
Dina Omar <dinaomar489@gmail.com>
2026-09-22 17:53:43 +0300

    "add enums for PGVector configuration

--- src/stores/vectordb/vectorDBEnums.py
+++ src/stores/vectordb/vectorDBEnums.py
@@ -2,11 +2,25 @@ from enum import Enum
 
 
 class VectorDBEnums(Enum):
-
     QDRANT = "QDRANT"
-
+    PGVECTOR = "PGVECTOR"
 
 class DistanceMethodEnums(Enum):
-
     COSINE = "cosine"
     DOT = "dot"
+
+class PGVectorTableschemaEnums(Enum):
+    ID = "id"
+    VECTOR = "vector"
+    TEXT = "text"
+    METADATA = "metadata"
+    _PREFIX = "pgvector_"
+    CHUNK_ID = "chunk_id"
+
+class PGVectorDistanceMethodEnums(Enum):
+    COSINE = "vector_cosine_ops"
+    DOT = "vector_l2_ops"
+
+class PGVectorIndexTypeEnums(Enum):
+    IVFFLAT = "ivfflat"
+    HNSW = "hnsw"
