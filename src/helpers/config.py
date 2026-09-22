3b18c63cfae64733b06a1c2f2e2e9d4ec0a5a91b
Dina Omar <dinaomar489@gmail.com>
2026-09-22 19:14:18 +0300

    update config file

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import List
 
 class Settings(BaseSettings):
     APP_NAME: str
@@ -8,8 +9,12 @@ class Settings(BaseSettings):
     FILE_MAX_SIZE: int
     FILE_CHUNLK_SIZE: int
 
-    MONGODB_URL: str
-    MONGODB_DATABASE:str
+
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USERNAME: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_MAIN_DATABASE: str
 
     GENERATION_BACKEND: str
     EMBEDDING_BACKEND: str
@@ -27,9 +32,11 @@ class Settings(BaseSettings):
     OPENAI_URL: str
     OPENAI_API_KEY: str
 
+    VECTOR_DB_BACKEND_LITERAL: List["str"] = None
     VECTOR_DB_BACKEND: str
     VECTOR_DB_PATH: str
     VECTOR_DB_DISTANCE_METHOD: str
+    VECTOR_DB_PGVEC_INDEX_THREADHOLD: int=100
 
     PRIMARY_LANG: str = "en"
     DEFAULT_LANG: str = "en"
